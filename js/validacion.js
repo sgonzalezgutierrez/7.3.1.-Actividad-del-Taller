94947a674af73c689d5a7aa6d3aff533a826b7f7
camilalazarozubillaga <furicafurica697@gmail.com>
2025-08-13 13:33:20 -0300

    CL - password-match-validation

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,15 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+function validarPasswordsIguales() {
+    const password1 = document.getElementById('password1').value;
+    const password2 = document.getElementById('password2').value;
+
+    if (password1 === password2) {
+      alert('Las contraseñas coinciden');
+      return true;
+    } else {
+      alert('Las contraseñas no coinciden');
+      return false;
+    }
+}
